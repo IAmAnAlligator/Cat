795d0ecc0f5cc4d54eb1eace64b1d63d8ebe92d8
IAmAnAlligator <torn90@gmail.com>
2024-04-28 19:24:31 +0300

    Second commit

--- main.c
+++ main.c
@@ -4,82 +4,123 @@
 // https://www.opennet.ru/man.shtml?topic=getopt_long&category=3&russian=0
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 
-int main (int argc, char **argv) {
-    int c;
-    int digit_optind = 0;
+void printBuffer(char *buffer) {
+    if (buffer) {
+        int ch;
+        int i = 0;
+        do {
+            ch = buffer[i];
+            printf("%c", ch);
+            i++;
+        } while (ch != '\0');
+    }
+}
 
+char *readFile(char *name) {
+    long length;
+    char *buffer = 0;
+
+    FILE *f = fopen(name, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        buffer = malloc(length + 100); // плюс место для добавочных символов
+        if (buffer) {
+            fread(buffer, 1, length, f);
+            buffer[length] = '\0';
+        }
+
+        fclose(f);
+    }
+
+    return buffer;
+}
+
+void numberLines(char *buffer) {
+}
+
+void addEnd(char *buffer) {
+    strcat(buffer, "$");
+}
+
+
+int main(int argc, char **argv) {
     while (1) {
-
-        int this_option_optind = optind ? optind : 1;
-
         int option_index = 0;
 
         static struct option long_options[] = {
             {"number-nonblank", 0, 0, 'b'},
             {"number", 0, 0, 'n'},
-            {"squeeze-blank", 0, 0, 's'},
-            {0, 0, 0, 0}
+            {"squeeze-blank", 0, 0, 's'}
         };
 
-        c = getopt_long (argc, argv, "benst012", long_options, &option_index);
+        int c = getopt_long(argc, argv, "bevEnstT", long_options, &option_index);
 
-        if (c == -1)
+        if (c == -1) {
             break;
-
+        }
 
         switch (c) {
-            case 0:
-                printf ("Parameter %s", long_options[option_index].name);
-            if (optarg)
-                printf (" with argument %s", optarg);
-            printf ("\n");
-            break;
-
-            case '0':
-            case '1':
-            case '2':
-                if (digit_optind != 0 && digit_optind != this_option_optind)
-                    printf ("цифры используются в двух разных элементах argv.\n");
-            digit_optind = this_option_optind;
-            printf ("параметр  %c\n", c);
-            break;
-
             case 'b':
-                printf ("parameter b\n");
-            break;
+                printf("parameter b\n");
+                break;
 
             case 'e':
-                printf ("parameter e\n");
-            break;
+                printf("parameter e\n");
+                break;
+
+            case 'v':
+                printf("parameter v\n");
+                break;
+
+            case 'E':
+                printf("parameter e\n");
+                break;
 
             case 'n':
-                printf ("parameter n\n");
-            break;
+                printf("parameter n\n");
+                break;
 
             case 's':
-                printf ("parameter s\n");
-            break;
+                printf("parameter s\n");
+                break;
 
             case 't':
-                printf ("parameter t\n");
-            break;
+                printf("parameter t\n");
+                break;
+
+            case 'T':
+                printf("parameter T\n");
+                break;
 
             case '?':
+                printf("Undefined\n");
                 break;
 
             default:
-                printf ("?? getopt возвратило код символа 0%o ??\n", c);
+                printf("?? getopt 0%o ??\n", c);
         }
     }
 
+    char *buffer = 0;
+
     if (optind < argc) {
-        printf ("Filename: ");
-        while (optind < argc)
-            printf ("%s ", argv[optind++]);
-        printf ("\n");
+        while (optind < argc) {
+            //printf ("%s\n", argv[optind++]);
+            buffer = readFile(argv[optind++]);
+        }
     }
+
+    addEnd(buffer);
+    printBuffer(buffer);
+
+    free(buffer);
 
     return 0;
 }
